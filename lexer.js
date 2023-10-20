b9e0d3956b05bf35ffcb31d90522a0b0060ffd09
darthvader1925 <elijb12@gmail.com>
2023-10-19 18:08:26 -0700

    lexeR

--- lexer.js
+++ lexer.js
@@ -2,52 +2,49 @@ const input = "777" // dev input
 
 function* lexer(str) {
     let i = 0;
-    let char = undefined;
+    let char = str[i];
+
+    function next() {
+        i++;
+        char = str[i];
+    }
 
     const number = () => {
-        let value = ""
-        for (; i <= str.length; i++) {
-            char = str[i]
-            if (char === "7") {
-                value += char;
-            } else {
-                break
-            }
+        let buffer = "";
+        while (char === "7") {
+            buffer += char;
+            next()
         }
 
-        if (value.length >= 1) {
+        if (buffer.length >= 1) {
             return {
                 type: "number",
-                value,
+                value: buffer
             }
         }
-
-        return null;
     }
     
     const eof = () => {
         char = str[i]; // time of video 1:08:16
         if (char == undefined) {
             i++;
-            return {
-                type: "EOF"
-            };
+            return { type: "EOF" };
+
         }
         
     }
 
-    for (; i <= str.length;) {
+    for (;;) {
         const token = number() || eof() || null; // if not a number, or eof, then undefined character
 
         if (token) {
             yield token;
+
+            if (token.type === "EOF") break;
         } else {
             throw new SyntaxError(`Unexpected character "${char}" at ${i + 1}`) // throw custom error later
         }
     }
 }
 
-console.log("starting");
-for (const token of lexer(input)) {
-    console.log(token);
-}
+console.log([...lexer(input)]);
